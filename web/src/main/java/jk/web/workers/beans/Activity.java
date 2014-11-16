bb03c62dbb05a5d27393fa8189e37210dd7533ca
Oleksandr Potomkin <11790587+OlexDragon@users.noreply.github.com>
2014-11-16 00:10:38 +0000

    activity

---
+++ web/src/main/java/jk/web/workers/beans/Activity.java
@@ -0,0 +1,33 @@
+package jk.web.workers.beans;
+
+import java.util.List;
+
+public class Activity {
+
+	private String title;
+	private String activityType;
+	private List<String> paragraphs;
+
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getActivityType() {
+		return activityType;
+	}
+	public void setActivityType(String activityType) {
+		this.activityType = activityType;
+	}
+	public List<String> getParagraphs() {
+		return paragraphs;
+	}
+	public void setParagraphs(List<String> paragraphs) {
+		this.paragraphs = paragraphs;
+	}
+	@Override
+	public String toString() {
+		return "ActivityBean [title=" + title + ", activityType=" + activityType + ", paragraphs=" + paragraphs + "]";
+	}
+}
