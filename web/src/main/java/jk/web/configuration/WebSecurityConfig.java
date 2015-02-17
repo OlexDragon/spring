88f5239e89a5aaf53609ed8f420d1e5b450b1b92
OlexDragon <OlexDragon@3c6001d0-9375-45c0-9b64-e33ed702c96e>
2015-02-17 20:01:04 +0000

    Search CareerjetApi

--- web/src/main/java/jk/web/configuration/WebSecurityConfig.java
+++ web/src/main/java/jk/web/configuration/WebSecurityConfig.java
@@ -52,6 +52,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				"/signup/**",
 				"/confirm/**",
 				"/search/**",
+				"/careerjet/**",
 				"/css/**",
 				"/js/**",
 				"/images/**",
--- web/src/main/java/jk/web/workers/search/controllers/CareerjetAPIController.java
+++ web/src/main/java/jk/web/workers/search/controllers/CareerjetAPIController.java
@@ -5,12 +5,11 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
-import net.minidev.json.JSONArray;
-import net.minidev.json.JSONObject;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +19,6 @@ import org.springframework.web.servlet.LocaleResolver;
 import com.careerjet.webservices.api.Client;
 
 @Controller
-@RequestMapping("/careerjet")
 public class CareerjetAPIController {
 
 	private final Logger logger = LogManager.getLogger();
@@ -28,39 +26,50 @@ public class CareerjetAPIController {
 	@Autowired
 	private LocaleResolver localeResolver;
 
-	@RequestMapping
-	public ResponseEntity<byte[]> search(
+	@RequestMapping("/careerjet")
+	public ResponseEntity<Object> search(
 									@RequestParam(value = "keywords") String keywords,
 									@RequestParam(value = "location", required=false) String location,
 									HttpServletRequest request){
 
 		logger.entry(keywords, location);
-		Client c = new Client(localeResolver.resolveLocale(request).toString());
-		  Map<String, String> args = new HashMap<String, String>();
-		   args.put("keywords", keywords);
+//		Locale locale = localeResolver.resolveLocale(request);
+		Client c = new Client("en_CA");
+		Map<String, String> args = new HashMap<String, String>();
+		args.put("keywords", keywords);
 
-		   if(location!=null)
-			   args.put("location", location);
+		if(location!=null)
+			args.put("location", location);
 
-//		   JSONObject results = (JSONObject) c.search(args);
-//
-//		   String type = (String)results.get("type");
-//		   switch(type){
-//		   case "JOBS":
+		JSONObject results = (JSONObject) c.search(args);
+
+		Object result;
+		HttpStatus status;
+		String type = (String)results.get("type");
+		switch(type){
+		case "JOBS":
+			   result = results.get("jobs");
+			   status = HttpStatus.OK;
 //			   JSONArray jobs = (JSONArray) results.get("jobs");
 //			   logger.trace("\n\t{}", jobs);
-//			   break;
-//		   case "LOCATIONS":
+			   break;
+		   case "LOCATIONS":
+			   result = results.get("solveLocations");
+			   status = HttpStatus.OK;
 //		       JSONArray solvelocations = (JSONArray) results.get("solveLocations");
 //			   logger.trace("\n\t{}", solvelocations);
-//			   break;
-//		   case "ERROR":
+			   break;
+		   case "ERROR":
+			   result = results.get("ERROR");
+			   status = HttpStatus.OK;
 //			   logger.error("\n\t Error: {}", results.get("ERROR"));
-//			   break;
-//		   default:
-//			   logger.trace("\n\t Type: {}", type);
-//		   }
+			   break;
+		   default:
+			   result = type;
+			   status = HttpStatus.OK;
+			   logger.trace("\n\t Type: {}", type);
+		   }
 
-		return null;
+		return logger.exit(new ResponseEntity<>(result,  status));
 	}
 }
