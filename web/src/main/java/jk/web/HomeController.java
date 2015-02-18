b325f1a260308691f528bfd4974e7e8e93365aa5
Oleksandr Potomkin <11790587+OlexDragon@users.noreply.github.com>
2015-02-18 04:26:06 +0000

    option-carriere ajax result

--- web/src/main/java/jk/web/HomeController.java
+++ web/src/main/java/jk/web/HomeController.java
@@ -1,15 +1,13 @@
 package jk.web;
 
-import jk.web.user.User;
-import jk.web.user.controllers.SignupController;
-import jk.web.user.repository.TitleRepository;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,13 +15,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @ComponentScan
 public class HomeController {
 
-	@Autowired
-	private TitleRepository titleRepository;
+	private final Logger logger = LogManager.getLogger();
 
 	@RequestMapping({ "/", "/home", "/index" })
-	public String home(User user, Model model) {
-		SignupController.signupAttributes(model, titleRepository);
-		model.addAttribute("message", "home.welcome");
+	public String home(Model model, @CookieValue(value="location", required=false) String locationCooky) {
+		logger.entry( locationCooky);
+
+		model.addAttribute("location", locationCooky);
 		return "search";
 	}
 
