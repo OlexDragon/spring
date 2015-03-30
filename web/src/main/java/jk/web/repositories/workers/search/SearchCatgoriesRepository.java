862cb34ed82ac19869e2a15c11f55ba1e64e6cb6
OlexDragon <OlexDragon@3c6001d0-9375-45c0-9b64-e33ed702c96e>
2015-03-30 01:43:18 +0000

    Added: manager/search/categories page

--- web/src/main/java/jk/web/repositories/workers/search/SearchCatgoriesRepository.java
+++ web/src/main/java/jk/web/repositories/workers/search/SearchCatgoriesRepository.java
@@ -2,20 +2,27 @@ package jk.web.repositories.workers.search;
 
 import java.util.List;
 
-import jk.web.entities.workers.search.SearchCatgoriesEntity;
-import jk.web.entities.workers.search.SearchCatgoriesEntity.CategoryStatus;
+import jk.web.entities.workers.search.SearchCatgoryEntity;
+import jk.web.entities.workers.search.SearchCatgoryEntity.CategoryStatus;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface SearchCatgoriesRepository extends JpaRepository<SearchCatgoriesEntity, Long>{
+public interface SearchCatgoriesRepository extends JpaRepository<SearchCatgoryEntity, Long>{
 
-	@Query("SELECT sc FROM SearchCatgoriesEntity sc WHERE (sc.categoryName LIKE CONCAT('\"',:startWith,'%') OR sc.categoryName LIKE CONCAT(:startWith,'%')) AND sc.status = :categoryStatus")
-	public List<SearchCatgoriesEntity> findByCategoryNameStartingWithAndStatus(@Param("startWith") String startWith, @Param("categoryStatus") CategoryStatus categoryStatus);
+	@Query("SELECT sc FROM SearchCatgoryEntity sc WHERE (sc.categoryName LIKE CONCAT('\"',:startWith,'%') OR sc.categoryName LIKE CONCAT(:startWith,'%')) AND sc.status = :categoryStatus")
+	public List<SearchCatgoryEntity> findByCategoryNameStartingWithAndStatus(@Param("startWith") String startWith, @Param("categoryStatus") CategoryStatus categoryStatus);
 
-	public List<SearchCatgoriesEntity> findByStatus(CategoryStatus categoryStatus);
+	public List<SearchCatgoryEntity> findByStatus(CategoryStatus categoryStatus);
 
-	@Query(value="SELECT DISTINCT UCASE(IF(SUBSTRING(`category_name`, 1, 1) = '\"', SUBSTRING(`category_name`, 2, 1),  SUBSTRING(`category_name`, 1, 1))) s FROM `jk`.`search_catgories` WHERE `category_status`=0", nativeQuery = true)
+	@Query(value="SELECT DISTINCT UCASE(IF(SUBSTRING(`category_name`, 1, 1) = '\"', SUBSTRING(`category_name`, 2, 1),  SUBSTRING(`category_name`, 1, 1))) s FROM `jk`.`search_categories` WHERE `category_status`=1", nativeQuery = true)
 	public List<String> findAvailableFirstLeters();
+
+	@Query(value="SELECT DISTINCT SUBSTRING(sce.categoryName, 1, 1) FROM SearchCatgoryEntity sce")
+	public List<String> findAvailableFirstCharacters();
+
+	public SearchCatgoryEntity findOneByCategoryName(String name);
+
+	public List<SearchCatgoryEntity> findByCategoryNameStartingWith(String startWith);
 }
